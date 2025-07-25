bf42982c0e0d81a5fa8c625ab9c545281f00f2df
Dodi Aditya <dodiaridharma@gmail.com>
2025-07-25 18:38:59 +0800

    chore: add type checking pre commit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-const { data } = await useFetch("/api/auth/login");
-console.log("🚀 ~ data:", data.value);
-</script>
-
 <template>
-  <h1>{{ JSON.stringify(data) }}</h1>
+  <h1>hello world</h1>
 </template>
